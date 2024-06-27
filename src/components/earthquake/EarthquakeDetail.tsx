c805c9198f2d34dd6f47284ed1aec73f9e6448c5
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-06-27 14:25:39 +0800

    ⚒️build: add dialog shakemap

--- src/components/earthquake/EarthquakeDetail.tsx
+++ src/components/earthquake/EarthquakeDetail.tsx
@@ -1,4 +1,5 @@
-import { Card, CardBody, Typography } from '@material-tailwind/react';
+import { Card, CardBody, Dialog, Typography } from '@material-tailwind/react';
+import { useState } from 'react';
 
 import { NewestEarthquake } from '../../interface';
 import EarthquakeDetailSkeleton from '../skeleton/EarthquakeDetailSkeleton';
@@ -8,55 +9,67 @@ interface EarthquakeDetailProps {
 }
 
 export default function EarthquakeDetail({ earthquake }: EarthquakeDetailProps) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen((prev) => !prev);
+
   if (!earthquake) {
     return <EarthquakeDetailSkeleton />;
   }
 
   return (
-    <Card className='rounded-lg border p-4 shadow-md'>
-      <Typography variant='h1' className='mb-4 text-center text-2xl font-bold'>
-        Info Gempa Terbaru
-      </Typography>
-      <CardBody className='flex flex-col lg:flex-row'>
-        <section className='flex flex-[1] flex-col justify-evenly px-4 lg:px-10'>
-          <Typography variant='h2' className='mb-2 text-xl font-semibold'>
-            {earthquake.wilayah}
-          </Typography>
+    <>
+      <Card className='rounded-lg border p-4 shadow-md'>
+        <Typography variant='h1' className='mb-4 text-center text-2xl font-bold'>
+          Info Gempa Terbaru
+        </Typography>
+        <CardBody className='flex flex-col lg:flex-row'>
+          <section className='flex flex-[1] flex-col justify-evenly px-4 lg:px-10'>
+            <Typography variant='h2' className='mb-2 text-xl font-semibold'>
+              {earthquake.wilayah}
+            </Typography>
 
-          <Typography variant='paragraph'>
-            <span className='font-bold'>Tanggal:</span> {earthquake.tanggal}
-          </Typography>
-          <Typography variant='paragraph'>
-            <span className='font-bold'>Jam:</span> {earthquake.jam}
-          </Typography>
-          <Typography variant='paragraph'>
-            <span className='font-bold'>Latitude:</span> {earthquake.lintang}
-          </Typography>
-          <Typography variant='paragraph'>
-            <span className='font-bold'>Longitude:</span> {earthquake.bujur}
-          </Typography>
-          <Typography variant='paragraph'>
-            <span className='font-bold'>Magnitude:</span> {earthquake.magnitude}
-          </Typography>
-          <Typography variant='paragraph'>
-            <span className='font-bold'>Kedalaman:</span> {earthquake.kedalaman}
-          </Typography>
-          <Typography variant='paragraph'>
-            <span className='font-bold'>Potensi:</span> {earthquake.potensi}
-          </Typography>
-          <Typography variant='paragraph'>
-            <span className='font-bold'>Dirasakan (Skala MMI):</span>{' '}
-            {earthquake.dirasakan}
-          </Typography>
-        </section>
-        <section className='m-auto w-full max-w-[450px]'>
-          <img
-            src={`https://data.bmkg.go.id/DataMKG/TEWS/${earthquake.shakemap}`}
-            alt='Shakemap'
-            className='mt-2 h-auto w-full'
-          />
-        </section>
-      </CardBody>
-    </Card>
+            <Typography variant='paragraph'>
+              <span className='font-bold'>Tanggal:</span> {earthquake.tanggal}
+            </Typography>
+            <Typography variant='paragraph'>
+              <span className='font-bold'>Jam:</span> {earthquake.jam}
+            </Typography>
+            <Typography variant='paragraph'>
+              <span className='font-bold'>Latitude:</span> {earthquake.lintang}
+            </Typography>
+            <Typography variant='paragraph'>
+              <span className='font-bold'>Longitude:</span> {earthquake.bujur}
+            </Typography>
+            <Typography variant='paragraph'>
+              <span className='font-bold'>Magnitude:</span> {earthquake.magnitude}
+            </Typography>
+            <Typography variant='paragraph'>
+              <span className='font-bold'>Kedalaman:</span> {earthquake.kedalaman}
+            </Typography>
+            <Typography variant='paragraph'>
+              <span className='font-bold'>Potensi:</span> {earthquake.potensi}
+            </Typography>
+            <Typography variant='paragraph'>
+              <span className='font-bold'>Dirasakan (Skala MMI):</span>{' '}
+              {earthquake.dirasakan}
+            </Typography>
+          </section>
+          <button className='m-auto w-full max-w-[450px]' onClick={handleOpen}>
+            <img
+              src={`https://data.bmkg.go.id/DataMKG/TEWS/${earthquake.shakemap}`}
+              alt='Shakemap'
+              className='mt-2 h-auto w-full cursor-pointer overflow-hidden transition-opacity hover:opacity-90'
+            />
+          </button>
+        </CardBody>
+      </Card>
+      <Dialog size='sm' open={open} handler={handleOpen}>
+        <img
+          src={`https://data.bmkg.go.id/DataMKG/TEWS/${earthquake.shakemap}`}
+          alt='Shakemap'
+          className='h-auto w-full'
+        />
+      </Dialog>
+    </>
   );
 }
