62b19e215f4e5befc93aaa7f492adec799b51f39
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-06-28 10:50:57 +0800

    ⚒️build: add fetch weather data

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,12 @@
-import { EarthquakeData, NewestEarthquake } from './interface';
+import {
+  Area,
+  EarthquakeData,
+  Issue,
+  NewestEarthquake,
+  Parameter,
+  Timerange,
+  WeatherData,
+} from './interface';
 
 const parseEarthquakeData = (xmlDoc: XMLDocument) => {
   const gempaElements = Array.from(xmlDoc.getElementsByTagName('gempa'));
@@ -28,6 +36,68 @@ const parseEarthquakeData = (xmlDoc: XMLDocument) => {
       dirasakan,
     };
   });
+};
+
+export const parseWeatherData = (xmlDoc: XMLDocument): WeatherData => {
+  const issueElement = xmlDoc.getElementsByTagName('issue')[0];
+
+  const issue: Issue = {
+    timestamp: issueElement.getElementsByTagName('timestamp')[0].textContent || '',
+    year: issueElement.getElementsByTagName('year')[0].textContent || '',
+    month: issueElement.getElementsByTagName('month')[0].textContent || '',
+    day: issueElement.getElementsByTagName('day')[0].textContent || '',
+    hour: issueElement.getElementsByTagName('hour')[0].textContent || '',
+    minute: issueElement.getElementsByTagName('minute')[0].textContent || '',
+    second: issueElement.getElementsByTagName('second')[0].textContent || '',
+  };
+
+  const areas = Array.from(xmlDoc.getElementsByTagName('area')).map((area) => {
+    const parameters = Array.from(area.getElementsByTagName('parameter')).map(
+      (parameter) => {
+        const timeranges = Array.from(parameter.getElementsByTagName('timerange')).map(
+          (timerange) => {
+            const values = Array.from(timerange.getElementsByTagName('value')).map(
+              (value) => ({
+                unit: value.getAttribute('unit') || '',
+                value: value.textContent || '',
+              }),
+            );
+
+            return {
+              type: timerange.getAttribute('type') || '',
+              datetime: timerange.getAttribute('datetime') || '',
+              hour: timerange.getAttribute('h'),
+              day: timerange.getAttribute('day'),
+              values,
+            } as Timerange;
+          },
+        );
+
+        return {
+          id: parameter.getAttribute('id') || '',
+          description: parameter.getAttribute('description') || '',
+          type: parameter.getAttribute('type') || '',
+          timeranges,
+        } as Parameter;
+      },
+    );
+
+    return {
+      id: area.getAttribute('id') || '',
+      name: area.getElementsByTagName('name')[0].textContent || '',
+      latitude: area.getAttribute('latitude') || '',
+      longitude: area.getAttribute('longitude') || '',
+      description: area.getAttribute('description') || '',
+      domain: area.getAttribute('domain') || '',
+      type: area.getAttribute('type') || '',
+      parameters,
+    } as Area;
+  });
+
+  return {
+    issue,
+    areas,
+  };
 };
 
 export const fetchNewestEarthquake = async (): Promise<NewestEarthquake | null> => {
@@ -93,6 +163,21 @@ export const fetchEarthquakeFelt = async (): Promise<EarthquakeData[] | null> =>
     return parseEarthquakeData(xmlDoc);
   } catch (error) {
     console.error('Error fetching earthquake data:', error);
+    return null;
+  }
+};
+
+export const fetchWeather = async (): Promise<WeatherData | null> => {
+  const url =
+    'https://data.bmkg.go.id/DataMKG/MEWS/DigitalForecast/DigitalForecast-Indonesia.xml';
+  try {
+    const response = await fetch(url);
+    const text = await response.text();
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(text, 'application/xml');
+    return parseWeatherData(xmlDoc);
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
     return null;
   }
 };
--- src/interface.ts
+++ src/interface.ts
@@ -23,3 +23,50 @@ export interface EarthquakeData {
   potensi?: string;
   dirasakan?: string;
 }
+
+// weather
+interface Value {
+  unit: string;
+  value: string;
+}
+
+export interface Timerange {
+  type: string;
+  datetime: string;
+  hour: string | null;
+  minute: string | null;
+  values: Value[];
+}
+
+export interface Parameter {
+  id: string;
+  description: string;
+  type: string;
+  timeranges: Timerange[];
+}
+
+export interface Issue {
+  timestamp: string;
+  year: string;
+  month: string;
+  day: string;
+  hour: string;
+  minute: string;
+  second: string;
+}
+
+export interface Area {
+  id: string;
+  name: string;
+  latitude: string;
+  longitude: string;
+  description: string;
+  domain: string;
+  type: string;
+  parameters: Parameter[];
+}
+
+export interface WeatherData {
+  issue: Issue;
+  areas: Area[];
+}
